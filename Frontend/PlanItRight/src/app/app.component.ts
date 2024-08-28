172e42437cda039b3e99fc05ba9cfaf17d790615
vivek-unni <2002vivekunni2002@gmail.com>
2024-08-28 19:16:57 +0530

    landing page and resp nav

--- Frontend/PlanItRight/src/app/app.component.ts
+++ Frontend/PlanItRight/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomepageComponent } from "./home/homepage/homepage.component";
+import { EventsComponent } from "./user/events/events.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomepageComponent, EventsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Frontend/PlanItRight/src/app/home/homepage/homepage.component.ts
@@ -0,0 +1,12 @@
+import { Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  standalone: true,
+  imports: [],
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.css'
+})
+export class HomepageComponent {
+  
+}
