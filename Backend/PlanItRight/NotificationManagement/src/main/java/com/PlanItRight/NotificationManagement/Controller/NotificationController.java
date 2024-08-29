ca1dbf484ab3c82b9bfd3f9f5f5aa4224da56b8f
kiran <rkirankumar761@gmail.com>
2024-08-30 00:15:25 +0530

    Sending Mail Implemented

---
+++ Backend/PlanItRight/NotificationManagement/src/main/java/com/PlanItRight/NotificationManagement/Controller/NotificationController.java
@@ -0,0 +1,22 @@
+package com.PlanItRight.NotificationManagement.Controller;
+
+import com.PlanItRight.NotificationManagement.Service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notification")
+public class NotificationController {
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @GetMapping("/sendEmail")
+    public String sendEmail(){
+
+        notificationService.sendEmail("bjornironside176@gmail.com","Testing","Testing");
+        return "Success";
+    }
+}
