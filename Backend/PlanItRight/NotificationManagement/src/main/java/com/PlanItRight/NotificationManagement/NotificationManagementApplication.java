791193db667fef1fb93e51705626fde27682d1ce
kiran <rkirankumar761@gmail.com>
2024-08-29 23:29:24 +0530

    Added new Service Notification Management

---
+++ Backend/PlanItRight/NotificationManagement/src/main/java/com/PlanItRight/NotificationManagement/NotificationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.PlanItRight.NotificationManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationManagementApplication.class, args);
+	}
+
+}
