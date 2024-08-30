71f9aa2bac53b3ed7e590a3c1db3f4ef47774dd7
kiran <rkirankumar761@gmail.com>
2024-08-30 23:32:16 +0530

    Gateway service is added

---
+++ Backend/PlanItRight/Gateway/src/test/java/com/ust/ApiGateway/HelloGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- Backend/PlanItRight/NotificationManagement/src/main/java/com/PlanItRight/NotificationManagement/Controller/NotificationController.java
+++ Backend/PlanItRight/NotificationManagement/src/main/java/com/PlanItRight/NotificationManagement/Controller/NotificationController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/notification")
+@RequestMapping("/notifications")
 public class NotificationController {
 
     @Autowired
