cfd4583e4825ca0d185bab5d1d77fbe3ca13c1fa
vivek-unni <2002vivekunni2002@gmail.com>
2024-08-30 22:23:39 +0530

    Guest List Component Completed but design can be improved much more

--- Frontend/PlanItRight/src/app/app.component.ts
+++ Frontend/PlanItRight/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomepageComponent } from "./home/homepage/homepage.component";
 import { EventsComponent } from "./user/events/events.component";
 import { DashboardComponent } from "./user/events/dashboard/dashboard.component";
+import { GuestlistComponent } from "./user/events/guestlist/guestlist.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomepageComponent, EventsComponent, DashboardComponent],
+  imports: [RouterOutlet, HomepageComponent, EventsComponent, DashboardComponent, GuestlistComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Frontend/PlanItRight/src/app/app.routes.ts
+++ Frontend/PlanItRight/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { EventsComponent } from './user/events/events.component';
 import { DashboardComponent } from './user/events/dashboard/dashboard.component';
+import { GuestlistComponent } from './user/events/guestlist/guestlist.component';
 
 export const routes: Routes = [
-    { path: 'events', component:EventsComponent },
-    {path:'dashboard',component:DashboardComponent}
-    
+    { path: 'events', component: EventsComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'guestlist', component: GuestlistComponent },
+
 ];
