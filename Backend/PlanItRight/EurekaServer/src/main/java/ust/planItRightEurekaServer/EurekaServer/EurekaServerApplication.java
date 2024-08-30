309094efaed6ff4ca50a90cbc8831eb5bd3316b5
kiran <rkirankumar761@gmail.com>
2024-08-30 23:15:07 +0530

    Added Discovery client in all services... All the services now visible in eureka client port no 8761

--- Backend/PlanItRight/EurekaServer/src/main/java/ust/planItRightEurekaServer/EurekaServer/EurekaServerApplication.java
+++ Backend/PlanItRight/EurekaServer/src/main/java/ust/planItRightEurekaServer/EurekaServer/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package ust.capstone.EurekaServer;
+package ust.planItRightEurekaServer.EurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
