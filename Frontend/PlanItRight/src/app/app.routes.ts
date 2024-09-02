864c0cefa2500af430d7112c060f638863648b51
vivek-unni <2002vivekunni2002@gmail.com>
2024-09-02 11:08:27 +0530

    Events integrated with backend

--- Frontend/PlanItRight/src/app/app.routes.ts
+++ Frontend/PlanItRight/src/app/app.routes.ts
@@ -2,10 +2,17 @@ import { Routes } from '@angular/router';
 import { EventsComponent } from './user/events/events.component';
 import { DashboardComponent } from './user/events/dashboard/dashboard.component';
 import { GuestlistComponent } from './user/events/guestlist/guestlist.component';
+import { ScheduleComponent } from './user/events/schedule/schedule.component';
+import { HomepageComponent } from './home/homepage/homepage.component';
+import { BudgetVendorsComponent } from './user/events/budget-vendors/budget-vendors.component';
 
 export const routes: Routes = [
     { path: 'events', component: EventsComponent },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'guestlist', component: GuestlistComponent },
-
+    { path: 'schedule', component: ScheduleComponent },
+    { path: 'budget', component: BudgetVendorsComponent },
+    { path: 'home', component: HomepageComponent},
+    { path: '**', redirectTo: '/home' },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
